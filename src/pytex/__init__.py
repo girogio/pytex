516316f1d1956812a315bf53597287bb8ef3e32a
girogio <giorgiogrigolo03@gmail.com>
2024-01-16 12:06:14 +0100

    Add version number

--- src/pytex/__init__.py
+++ src/pytex/__init__.py
@@ -3,3 +3,5 @@ from pytex.document import LatexDocument, PageType
 from pytex.package import Package
 from pytex.command import Command
 from pytex.config import LatexConfig
+
+__version__ = "1.0.0"
